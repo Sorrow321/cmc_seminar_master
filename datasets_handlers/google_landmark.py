8c0fb284a959e3b03ffc5c20b79ea600cec4ec77
Ilya Fedorov <ilyaf@nvidia.com>
2022-05-02 05:44:22 +0300

    good update

--- datasets_handlers/google_landmark.py
+++ datasets_handlers/google_landmark.py
@@ -2,17 +2,20 @@
 
 
 import pandas as pd
+import json
 from torch.utils.data import Dataset
 
 
 class google_landmark_dataset(Dataset):
     def __init__(self):
-        self.df = pd.read_csv('google-landmark/train.csv')
-    
+        self.df = pd.read_csv('data/google-landmark/train.csv')
+        with open('data/google-landmark/glm_mapping.json', 'rt') as file:
+            self.mapping = json.load(file)
+
     def __getitem__(self, idx):
         name = self.df.iloc[idx]['id']
         path = f'google-landmark/train/{name[0]}/{name[1]}/{name[2]}/{name}.jpg'
-        label = int(self.df.iloc[idx]['landmark_id'])
+        label = self.mapping[str(self.df.iloc[idx]['landmark_id'])]
         return path, label
 
     def __len__(self):
